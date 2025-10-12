d711dcaaed87c5fe9ad36d7db46f82572a08c6e4
Mahek Dudhat <mahekm452@gmail.com>
2025-10-12 10:58:39 +0530

    Made some changes in server.js file

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,7 @@ const verifyToken = (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) return res.status(403).json({ message: 'No token provided' });
 
-    jwt.verify(token.split(' ')[1], 'your_jwt_secret', (err, decoded) => {
+    jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, decoded) => {
         if (err) return res.status(401).json({ message: 'Unauthorized' });
         req.userId = decoded.id;
         req.isAdmin = decoded.isAdmin;
